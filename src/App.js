ded6ed363abbe95561c8f0e85aabdfd4b5c32270
Domo <stolarczyk.dominik@gmail.com>
2020-01-13 08:58:24 +0100

    HookCounter added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import './App.css';
 import ClassCounter from './components/ClassCounter';
+import HookCounter from './components/HookCounter';
 
 function App() {
 	return (
 		<div className="App">
+			<label>Class counter:</label>
 			<ClassCounter />
+
+			<label>UseState hook counter:</label>
+			<HookCounter />
 		</div>
 	);
 }
