c0c971c85e11ecaec6461261abc99e0bca30bc5e
Domo <stolarczyk.dominik@gmail.com>
2020-01-13 09:19:42 +0100

    HookCounterTwo added

--- src/App.js
+++ src/App.js
@@ -2,15 +2,21 @@ import React from 'react';
 import './App.css';
 import ClassCounter from './components/ClassCounter';
 import HookCounter from './components/HookCounter';
+import HookCounterTwo from './components/HookCounterTwo';
 
 function App() {
 	return (
 		<div className="App">
 			<label>Class counter:</label>
 			<ClassCounter />
-
+			<hr />
 			<label>UseState hook counter:</label>
 			<HookCounter />
+			<hr />
+			<label>UseState hook counter with previous state:</label>
+
+			<HookCounterTwo />
+			<hr />
 		</div>
 	);
 }
