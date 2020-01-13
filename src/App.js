98b09b51c749054423213821ce5d768056c12246
Domo <stolarczyk.dominik@gmail.com>
2020-01-13 10:13:35 +0100

    HookCounterFour added

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import ClassCounter from './components/ClassCounter';
 import HookCounter from './components/HookCounter';
 import HookCounterTwo from './components/HookCounterTwo';
 import HookCounterThree from './components/HookCounterThree';
+import HookCounterFour from './components/HookCounterFour';
 
 function App() {
 	return (
@@ -22,6 +23,8 @@ function App() {
 
 			<HookCounterThree />
 			<hr />
+
+			<HookCounterFour />
 		</div>
 	);
 }
