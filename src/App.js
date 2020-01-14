a32d560a6fb5aa76f86eb1b57c5e158da84ee07f
Domo <stolarczyk.dominik@gmail.com>
2020-01-14 08:53:54 +0100

    context + reducer

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useReducer } from 'react';
 import './App.css';
 import ClassCounter from './components/ClassCounter';
 import HookCounter from './components/HookCounter';
@@ -14,11 +14,30 @@ import ComponentA from './components/ComponentA';
 import ReducerCounter from './components/ReducerCounter';
 import ReducerCounterTwo from './components/ReducerCounterTwo';
 import ReducerCounterThree from './components/ReducerCounterThree';
+import Component1 from './components/Component1';
+import Component2 from './components/Component2';
+import Component3 from './components/Component3';
 
 export const UserContext = React.createContext();
 export const ChannelContext = React.createContext();
+export const CountContext = React.createContext();
+
+const initialState = 0;
+const reducer = (state, action) => {
+	switch (action) {
+		case 'increment':
+			return state + 1;
+		case 'decrement':
+			return state - 1;
+		case 'reset':
+			return initialState;
+		default:
+			return state;
+	}
+};
 
 function App() {
+	const [count, dispatch] = useReducer(reducer, initialState);
 	return (
 		<div className="App">
 			<h2>useState hook</h2>
@@ -30,14 +49,11 @@ function App() {
 			<HookCounter />
 			<hr />
 			<label>UseState hook counter with previous state:</label>
-
 			<HookCounterTwo />
 			<hr />
 			<label>UseState hook counter with an object:</label>
-
 			<HookCounterThree />
 			<hr />
-
 			<HookCounterFour />
 			<hr />
 			<hr />
@@ -70,6 +86,17 @@ function App() {
 			<ReducerCounterTwo />
 			<hr />
 			<ReducerCounterThree />
+			<hr />
+			<h2>useReducer + useContext</h2>
+			<hr />
+			Count - {count}
+			<CountContext.Provider
+				value={{ countState: count, countDispatch: dispatch }}
+			>
+				<Component1 />
+				<Component2 />
+				<Component3 />
+			</CountContext.Provider>
 		</div>
 	);
 }
